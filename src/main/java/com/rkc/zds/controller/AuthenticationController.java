6dd19043759e59a8c8ad9f3b233299bc7e3ddd6f
rcampion <rcampion@rcampion-virtual-machine>
2018-11-19 22:07:26 -0500

    CORS on authentication fix

--- src/main/java/com/rkc/zds/controller/AuthenticationController.java
+++ src/main/java/com/rkc/zds/controller/AuthenticationController.java
@@ -20,6 +20,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(value = "/api")
 public class AuthenticationController {
